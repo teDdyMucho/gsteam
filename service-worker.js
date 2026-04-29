3bcd0984d3a95bf8f755521570c302de9c6651bc
teDdyMucho <teDdyMucho@users.noreply.github.com>
2026-04-30 04:16:03 +0800

    Bump SW VERSION + CABT_BUILD for branded textarea/select polish deploy

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@
 //
 // 1777255980 is replaced at build time by scripts/build-pwa.mjs.
 
-const VERSION = '1777486228';
+const VERSION = '1777493800';
 const CACHE   = `cabt-${VERSION}`;
 
 // Files known at install time. Other same-origin requests are cached on first hit.
